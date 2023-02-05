a20730bcd73f7ffb6f74aa1a914e612eb8a9e62d
Shalimasince2003 <97387282+Shalimasince2003@users.noreply.github.com>
2023-02-05 22:12:27 +0530

    Create lastword.java

---
+++ lastword.java
@@ -0,0 +1,42 @@
+/*Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+A word is a maximal 
+substring
+ consisting of non-space characters only.
+
+ 
+
+Example 1:
+
+Input: s = "Hello World"
+Output: 5
+Explanation: The last word is "World" with length 5.
+Example 2:
+
+Input: s = "   fly me   to   the moon  "
+Output: 4
+Explanation: The last word is "moon" with length 4.
+Example 3:
+
+Input: s = "luffy is still joyboy"
+Output: 6
+Explanation: The last word is "joyboy" with length 6.*/
+class Solution {
+    public int lengthOfLastWord(String s) {
+        String s1=s.trim();
+        int count=0;
+        char c[]=s1.toCharArray();
+        
+        for(int i=c.length-1;i>=0;i--)
+        {
+            if(c[i]==' ')
+            {
+                break;
+            }
+            count++;
+        }
+        return count;
+        
+        
+    }
+}
